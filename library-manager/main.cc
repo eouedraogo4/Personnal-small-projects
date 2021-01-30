2d4892615af91f29773c7b5c45aa0898b7dba34b
eouedraogo4 <48145816+eouedraogo4@users.noreply.github.com>
2021-01-29 21:26:43 -0500

    implement functions and adding login logout. reading file not implemented yet

--- library-manager/main.cc
+++ library-manager/main.cc
@@ -2,12 +2,14 @@
 
 using namespace std;
 
+
 class User {
 	string id, password, name;
+	unordered_map<string, int> books;
 public:
-	User(string id, string password) {}
+	User(string id, string password) : User(id,password,"") {}
 
-	User(string id, string password, string name) {}
+	User(string id, string password, string name) : id(id), password(password), name(name) {}
 
 	vector<string> details() const { return { getid(), getpassword(), getname() }; }
 
@@ -16,92 +18,182 @@ public:
 	string getpassword() const { return password; }
 	
 	string getname() const { return name; }
+
+	void borrow(string barcode) {
+		unordered_map<string,int>::iterator it = books.find(barcode);
+		if(it == books.end()) {
+			books.insert(pair<string,int>(barcode,1));
+		}
+		else {
+			it->second++;
+		}
+	}
+
+	void back(string barcode) {
+		unordered_map<string,int>::iterator it = books.find(barcode);
+		if(it != books.end()) {
+			if(--it->second == 0) {
+				books.erase(barcode);
+			}
+		}
+	}
 };
 
-
-class Users {
-	unordered_map<string, User> Hash;
+class Book {
+	string barcode, name, author;
+	string* userid;
 public:
-	Users() {}
+	Book() : Book("","","") {}
 
-	Users(string file) {}
+	Book(string barcode, string name, string author) : barcode(barcode), name(name), author(author), userid(NULL) {}
 
-	Users(ifstream in) {}
+	string getbarcode() const { return barcode; }
 
-	vector<string> details(sting id) {
-		unorderd_map<string, User>::iterator it = Hash.find(id);
-		if(it == Hash.end())
-			return "not found";
-		return it->second.details();
-	}
+	string getname() const { return name; }
 
-	bool login(string id, string password) {
-		unorderd_map<string, User>::iterator it = Hash.find(id);
-		if(it == Hash.end())
-			return false;
-		return it->second.getpassword() == password;
-	}
+	string getauthor() const { return author; }
+
+	string* getuserid() { return userid; }
+
+	void setuserid(string* id) { userid = id; }
+
+	vector<string> details() const { return { barcode, name, author }; }
 };
+
 
 class Library {
-
+	unordered_map<string,Book*> books;
+	unordered_map<string,User*> users;
+	User* currUser;
 public:
 	Library() {}
 
-	Library(string file) {}
+	Library(string books, string users) {}
 
-	Library(ifstream in) {}
+	Library(ifstream books, ifstream users) {}
 
-	void Search(string book) {
-
+	void search(string key) {
+		unordered_map<string,Book*>::iterator it = books.find(key);
+		if(it == books.end()) {
+			cout << "Not found" << endl;
+		}
+		else {
+			Book* book = it->second;
+			if(!book->getuserid()) {
+				for(auto& e: book->details()) 
+					cout << e << endl;
+			}
+		}
 	}
 
-	void borrow(string id, string book) {
-
+	void borrow(string key) {
+		if(loggedin()) {
+			unordered_map<string,Book*>::iterator it = books.find(key);		
+			if(it == books.end()) {
+				cout << "Not found" << endl;
+			}
+			else {
+				Book* book = it->second;
+				if(!book->getuserid()) {
+					book->setuserid(new string(currUser->getid()));
+					currUser->borrow(book->getbarcode());
+				}
+				else {
+					cout << "Book already out" << endl;
+				}
+			}
+		}
+		else {
+			cout << "Not logged in" << endl;
+		}
 	}
 
-	void back(string book) {
+	void back(string key) {
+		unordered_map<string,Book*>::iterator it = books.find(key);
+		if(it == books.end()) {
+			cout << "Book Not found" << endl;
+		}
+		else {
+			Book* book = it->second;
+			string* userid = book->getuserid();
+			if(userid) {
+				unordered_map<string,User*>::iterator it = users.find(*userid);
+				if(it != users.end()) {
+					User* user = it->second;
+					user->back(book->getbarcode());
+					book->setuserid(NULL);
+				}
+			}
+		}
+	}
 
+	bool login(string id, string password) {
+		if(loggedin()) {
+			cout << "Already logged in, logout first" << endl;
+			return false;
+		}
+		
+		unordered_map<string, User*>::iterator it = users.find(id);
+		if(it != users.end() && it->second->getpassword() == password) {
+			currUser = it->second;
+			return true;
+		}
+		cout << "ID or Password not namtch !!!!!" << endl;
+		return false;
+	}
+
+	bool login() {
+		string id, password;
+		cout << "Login" << endl << "ID : ";
+		cin >> id;
+		cout << "Password : ";
+		cin >> password;
+		return login(id,password);
+	}
+
+	bool loggedin() { return currUser != NULL; }
+
+	void logout() { currUser = NULL; }
+
+	vector<string> userDetails() {
+		if(loggedin()) {
+			return currUser->details();
+		}
+		else {
+			return { "Not logged in" };
+		}
 	}
 };
 
 
 int main() {
-	Users* users = new Users("people.data");
-	Library* lib = new Library("books.data");
+	Library* lib = new Library("books.data","users.data");
 	while(true) {
 		int choice;
-		string id, s, password;
+		string s;
 		cout << "1. Search" << endl << "2. Query" << "3. Borrow" << endl << "4. Return" << endl << "5.Exit" << endl;
 		cin >> choice;
 		switch(choice) {
 			case 1:
 				cout << "Search by Book name, Barcode No, or Author : ";
 				cin >> s;
-				lib.search(s);
+				lib->search(s);
 				break;
 			case 2:
-				cout << "Enter your ID : ";
-				cin >> id;
-				for(auto& e : users.details(s))
+				while(!lib->login());
+				for(auto& e : lib->userDetails())
 				       cout << e << endl;
 				break;
 			case 3:
-				cout << "Login" << endl << "ID : ";
-				cin >> id;
-				cout << "Password : ";
-				cin >> password;
+				while(!lib->login());
 				cout << "Enter the book Barcode No : ";
 				cin >> s;
-				if(users.login(id,password))
-					lib.borrow(id,s);
-				else
-					cout << "Not registered" << endl;
+				lib->borrow(s);
 				break;
 			case 4:
 				cout << "Enter book Barcode No : ";
 				cin >> s;
-				lib.back(s);
+				lib->back(s);
 				break;
 			case 5:
 				return 0;
